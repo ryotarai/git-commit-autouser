5889e466fc984674e0b55e4147a80283b0d629ff
Ryota Arai <ryota.arai@gmail.com>
2013-06-02 07:26:39 +0900

    bundle gem git-commit-autouser

---
+++ lib/git/commit/autouser.rb
@@ -0,0 +1,9 @@
+require "git/commit/autouser/version"
+
+module Git
+  module Commit
+    module Autouser
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/git/commit/autouser/version.rb
@@ -0,0 +1,7 @@
+module Git
+  module Commit
+    module Autouser
+      VERSION = "0.0.1"
+    end
+  end
+end
