291f95e0222c14103541472784ac5d431f5a0bdd
Ryota Arai <ryota.arai@gmail.com>
2013-06-18 20:50:07 +0900

    Add specs of Git class.

--- spec/lib/git_commit_autouser/git_spec.rb
+++ spec/lib/git_commit_autouser/git_spec.rb
@@ -25,5 +25,35 @@ module GitCommitAutouser
         it { should == 'invalid' }
       end
     end
+
+    describe '.rewrite_committer_author' do
+      context 'when the repo has 1 commit' do
+        subject do
+          in_repo do |path|
+            `git commit --allow-empty -m 'Hello'`
+            described_class.rewrite_committer_author(name, email)
+            `git log | cat`
+          end
+        end
+        let(:name) { "John Lennon" }
+        let(:email) { "john.lennon@gmail.com" }
+        it { should include name }
+        it { should include email }
+      end
+      context 'when the repo has 1 commit' do
+        subject do
+          in_repo do |path|
+            `git commit --allow-empty -m 'Hello1'`
+            `git commit --allow-empty -m 'Hello2'`
+            described_class.rewrite_committer_author(name, email)
+            `git log | cat`
+          end
+        end
+        let(:name) { "John Lennon" }
+        let(:email) { "john.lennon@gmail.com" }
+        it { should include name }
+        it { should include email }
+      end
+    end
   end
 end
