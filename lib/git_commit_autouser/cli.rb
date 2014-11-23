bfe59301a8c7e2374cd31a8a7cf68c75b6eb9cf3
Ryota Arai <ryota.arai@gmail.com>
2014-11-23 16:36:30 +0900

    Do not abort even if origin url is not set.

--- lib/git_commit_autouser/cli.rb
+++ lib/git_commit_autouser/cli.rb
@@ -1,38 +1,15 @@
 module GitCommitAutouser
   class Cli
     def self.start(argv=ARGV)
-      abort_no_origin! unless Git.remote?("origin")
+      unless Git.remote?("origin")
+        $stderr.puts "[warn] remote `origin` is not configured"
+      end
+
       remote_url = Git.remote_push_url("origin")
 
       users = Config.users
-      abort_no_user_config! if users.empty?
-
-      matched = nil
-      users.each do |user|
-        matched = user.url_regexp.match(remote_url)
-        unless matched.nil?
-          matched = user
-          break
-        end
-      end
-
-      abort_no_user_matched! if matched.nil?
-
-      env = {
-        "GIT_COMMITTER_NAME" => matched.name,
-        "GIT_COMMITTER_EMAIL" => matched.email,
-        "GIT_AUTHOR_NAME" => matched.name,
-        "GIT_AUTHOR_EMAIL" => matched.email,
-      }
-
-      env["HUB_CONFIG"] = matched.hub_config
-
-      system env, "git", "commit", *argv
-      exit $?.exitstatus
-    end
-
-    def self.abort_no_user_config!
-      $stderr.puts <<-EOS
+      if users.empty?
+        $stderr.puts <<-EOS
 No user setting found. You should add to ~/.gitconfig like the following.
 ------
 [#{Config::USER_CONFIG_PREFIX}github]
@@ -44,19 +21,35 @@ No user setting found. You should add to ~/.gitconfig like the following.
   name = "Foo Bar"
   email = bar@company.com
 ------
-      EOS
-      exit 1
-    end
+        EOS
+        abort
+      end
 
-    def self.abort_no_origin!
-      $stderr.puts "remote `origin` is not configured"
-      exit 1
-    end
+      matched = nil
+      users.each do |user|
+        matched = user.url_regexp.match(remote_url)
+        unless matched.nil?
+          matched = user
+          break
+        end
+      end
 
-    def self.abort_no_user_matched!
-      $stderr.puts "No user setting matched. Abort."
-      $stderr.puts "Remote Url: #{remote_url}"
-      exit 1
+      if matched
+        env = {
+          "GIT_COMMITTER_NAME" => matched.name,
+          "GIT_COMMITTER_EMAIL" => matched.email,
+          "GIT_AUTHOR_NAME" => matched.name,
+          "GIT_AUTHOR_EMAIL" => matched.email,
+          "HUB_CONFIG" => matched.hub_config,
+        }
+      else
+        env = {}
+        $stderr.puts "[warn] No user setting matched. Abort."
+        $stderr.puts "[warn] Remote Url: #{remote_url}"
+      end
+
+      system env, "git", "commit", *argv
+      exit $?.exitstatus
     end
   end
 end
